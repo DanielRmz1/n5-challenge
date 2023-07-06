7cc2ba68343fd3c27dde310b945f37129a29047f
DanielRmz1 <daniel.1912@hotmail.com>
2023-07-06 13:07:15 -0600

    Add button component

---
+++ host-project/src/components/Button/index.tsx
@@ -0,0 +1,59 @@
+import React, { FC } from "react";
+import styled from "styled-components";
+
+interface StyleButtonProps {
+	secondary: boolean;
+	disabled: boolean;
+}
+
+const StyledButton = styled.button<StyleButtonProps>`
+	border: none;
+	outline: none;
+	font-size: 1rem;
+	padding: 0.75rem 1.5rem;
+	font-weight: 500;
+	border-radius: 0.25rem;
+	cursor: pointer;
+
+	${(props) =>
+		props.disabled &&
+		`opacity: 0.4; user-select: none; pointer-events: none;`}
+
+	background-color: var(
+		${(props) => (props.secondary ? "--accent-color" : "--primary-color-100")}
+	);
+	color: var(
+		${(props) =>
+			props.secondary ? "--primary-color" : "--secondary-text-color"}
+	);
+
+	&:hover {
+		background-color: var(
+			${(props) =>
+				props.secondary ? "--accent-color-100" : "--primary-color-200"}
+		);
+	}
+
+	&:active {
+		background-color: var(
+			${(props) =>
+				props.secondary ? "--accent-color-200" : "--primary-color-300"}
+		);
+	}
+`;
+
+interface ButtonProps {
+	secondary?: boolean;
+	children: string;
+	disabled?: boolean;
+}
+
+const Button: FC<ButtonProps> = ({ secondary, children, disabled }) => {
+	return (
+		<StyledButton secondary={secondary} disabled={disabled}>
+			{children}
+		</StyledButton>
+	);
+};
+
+export default Button;
--- host-project/src/views/Challenge/index.tsx
+++ host-project/src/views/Challenge/index.tsx
@@ -1,7 +1,21 @@
+import Button from "components/Button";
 import React, { FC } from "react";
 
 const Challenge: FC = () => {
-	return <div>Component</div>;
+	return (
+		<div>
+			<div>Challenge</div>
+			<div>Buttons</div>
+			<div>
+				<Button>Primary</Button>
+				<Button secondary>Secondary</Button>
+				<Button secondary disabled>
+					Disabled
+				</Button>
+				<Button disabled>Disabled 2</Button>
+			</div>
+		</div>
+	);
 };
 
 export default Challenge;
