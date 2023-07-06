c31753f4bb790696d315488298d86ec07aa63f0b
DanielRmz1 <daniel.1912@hotmail.com>
2023-07-06 13:31:01 -0600

    add initial config for remote project

---
+++ pokemon-remote/src/components/Pokemon/index.tsx
@@ -0,0 +1,17 @@
+import React from "react";
+import styled from "styled-components";
+
+const StyledPokemon = styled.div`
+	font-size: 2rem;
+	padding: 4rem;
+	border-radius: 1rem;
+	background-color: white;
+	border: 1px solid black;
+	max-width: 10rem;
+`;
+
+const Pokemon = () => {
+	return <StyledPokemon>Metapod</StyledPokemon>;
+};
+
+export default Pokemon;
--- pokemon-remote/vite.config.ts
+++ pokemon-remote/vite.config.ts
@@ -1,7 +1,23 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
+import federation from "@originjs/vite-plugin-federation";
+import tsChecker from "vite-plugin-checker";
+import tsconfigPaths from "vite-tsconfig-paths";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
-})
+	plugins: [
+		react(),
+		tsconfigPaths(),
+		tsChecker({ typescript: true }),
+		federation({
+			name: "pokemon",
+			filename: "pokemon.tsx",
+			exposes: {
+				"./Pokemon": "./src/components/Pokemon/index.tsx",
+			},
+			shared: ["react", "react-dom"],
+		}),
+	],
+});
+
