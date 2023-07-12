a7fc4953e278074eeae2cf8fa77877d52414389c
DanielRmz1 <daniel.1912@hotmail.com>
2023-07-11 20:26:07 -0600

    Add initial config

---
+++ pokemon-remote/src/components/Entity/components/Stat/index.test.tsx
@@ -0,0 +1,25 @@
+import React from "react";
+import { render, screen, waitFor } from "utils/test-utils";
+import Stat from ".";
+
+test("Check Stat component renders correctly simple component", () => {
+	const props = {
+		description: "some stat",
+		value: 10,
+	};
+	render(<Stat {...props} />);
+
+	screen.getByText(props.description + ":");
+	screen.getByText(props.value);
+});
+
+// https://pokeapi.co/api/v2/stat/1/
+test("Check Stat component renders correctly fetched stat", async () => {
+	const props = {
+		url: "https://pokeapi.co/api/v2/stat/1/",
+		value: 10,
+	};
+	render(<Stat {...props} />);
+	await waitFor(() => screen.getByText(/hp/i));
+	screen.debug();
+});
